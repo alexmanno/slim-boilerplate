9ef46c50d0ab1e7bb82584ff10181dcae55f116b
Alessandro Manno <alessandro@alessandromanno.info>
2017-11-16 00:14:00 +0100

    Improvements on index.php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ $app = new \Slim\App($container);
 
 $routes = scandir(__DIR__ . '/../app/Routes/');
 foreach ($routes as $route) {
-    if (is_file(__DIR__ . '/../app/Routes/' . $route) && pathinfo(__DIR__ . '/../app/Routes/' . $route, PATHINFO_EXTENSION)['extension'] === 'php')) {
+    if (is_file(__DIR__ . '/../app/Routes/' . $route) && mb_substr($route, -4, 4) === '.php') {
         require __DIR__ . '/../app/Routes/' . $route;
     }
 }
